1b27a48453907546812ffa80b7d5e5b23c262ae1
Abhishek Sarathe <abhisheksarathe2917@gmail.com>
2025-09-07 22:30:09 +0530

    Initial commit - React project

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    question: "Which device is required for internet connection?",
+    option1: "Modem",
+    option2: "Router",
+    option3: "LAN Cable",
+    option4: "Pen Drive",
+    ans: 1,
+  },
+  {
+    question: "Which continent has the largest population?",
+    option1: "Asia",
+    option2: "Europe",
+    option3: "North America",
+    option4: "Africa",
+    ans: 1,
+  },
+  {
+    question: "React is mainly used for building?",
+    option1: "Database",
+    option2: "User Interface",
+    option3: "Networking",
+    option4: "Operating System",
+    ans: 2,
+  },
+  {
+    question: "Which hook is used to manage state in React?",
+    option1: "useContext",
+    option2: "useState",
+    option3: "useEffect",
+    option4: "useRef",
+    ans: 2,
+  },
+  {
+    question: "Which language is used to style React applications?",
+    option1: "HTML",
+    option2: "CSS",
+    option3: "Python",
+    option4: "C++",
+    ans: 2,
+  }
+];
